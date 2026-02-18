470d6ca544b2168da9db64f3450d5848b09649cf
bobj0nov <prosto.akkaunt.n2@gmail.com>
2026-02-18 09:59:16 +0300

    Add files via upload

---
+++ Lesson 2 js/index.js
@@ -0,0 +1,14 @@
+let userName = "Kamron"
+let userAge = 13 
+let userEmail = "example@example.com"
+let userisAdmin = true
+
+console.log(userName)
+console.log(userAge)
+console.log(userEmail)
+console.log(userisAdmin)
+
+
+let num1 =prompt("Son 1")
+let num2 =prompt("Son 2")
+console.log(Number(num1) * Number(num2))
