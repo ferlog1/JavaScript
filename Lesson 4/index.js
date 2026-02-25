c42e497f81d29958c6503cfcb9ac4e0c4523c962
bobj0nov <prosto.akkaunt.n2@gmail.com>
2026-02-25 10:02:05 +0300

    Add files via upload

---
+++ Lesson 4/index.js
@@ -0,0 +1,4 @@
+let userAge = prompt("Yoshingiz")
+if(userAge >=13)
+{console.log("Siz katta yoshdasiz")}
+else{console.log("Siz kichik yoshdasiz")}
---
+++ Lesson 5/index.js
@@ -0,0 +1,9 @@
+let pasword = prompt("Kodni kiriting")
+if(pasword === "secret123")
+    {console.log("Siz tizimga muvafaqiyatli kirdingiz✅")
+
+    }
+else
+    {
+    console.log("Siz notog'ri kod kiritingiz❌")
+}
