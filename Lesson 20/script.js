2108aa6698e7baa64ee6bf4c8f20664ac06181b8
bobj0nov <prosto.akkaunt.n2@gmail.com>
2026-04-20 11:21:20 +0500

    Add files via upload

---
+++ Lesson 20/script.js
@@ -0,0 +1,23 @@
+// // let word = prompt("So'z kiriting:");
+
+// // let length = word.length;
+// // let upperWord = word.toUpperCase();
+
+// // console.log("Uzunligi:", length);
+// // console.log("Katta harflarda:", upperWord);
+
+// // let text = "JavaScript juda qiziqarli va kuchli dasturlash tilidir.";
+// // let hasKuchli = text.includes("kuchli");
+// // let indexJuda = text.indexOf("juda");
+// // console.log("kuchli bormi:", hasKuchli);   
+// // console.log("juda indeksi:", indexJuda);   
+
+// let email = "prosto.akkaunt.n2@gmail.com";
+// console.log(email.slice(17));
+
+
+
+let data = "   Olma, Behi, Uzum   ";
+let cleanData = data.trim();
+let result = cleanData.replace("Behi", "Anor");
+console.log(result);
