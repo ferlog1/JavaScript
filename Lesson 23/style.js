ca76904d6ce2908a702c1020cb5e3fb05b728635
bobj0nov <prosto.akkaunt.n2@gmail.com>
2026-05-06 10:35:01 +0500

    Add files via upload

---
+++ Lesson 23/style.js
@@ -0,0 +1,38 @@
+         
+         
+         // 1-MISOL
+// function sayHello(ism) 
+// {
+//   return "Assolomu Aleykum, " + ism + "!";
+// }
+
+// console.log(sayHello(prompt("ismingizni kriting")));
+
+
+
+
+
+            // 2-MISOL
+// function qoshiladi(a, b) 
+// {
+//   return  a + b;
+// }
+// console.log(qoshiladi(5, 11));
+
+
+
+            // 3-MISOL
+// function isEven(son) {
+//   return son % 2 === 0;
+// }
+
+// console.log(isEven(prompt("Sonni kiriting")));
+
+            
+            // 4-misol
+function ayirma(tugildi) {
+  return 2026 - tugildi;
+}
+console.log(ayirma(prompt("Yoshingizni kiriting"))); 
+
+
