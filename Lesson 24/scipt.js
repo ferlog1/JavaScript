afe6e087b97feee8677225292cd9f75c92cd29e4
bobj0nov <prosto.akkaunt.n2@gmail.com>
2026-05-06 12:01:24 +0500

    Add files via upload

---
+++ Lesson 24/scipt.js
@@ -0,0 +1,126 @@
+                
+
+
+
+                
+// // // // // //                 // 1-topshiriq 1-misol
+// // // // // // // // function samakm(ism, vaqt) {
+// // // // // // // //     return prompt;
+// // // // // // // // }
+// // // // // // // // let vaqt =prompt("Xozir qaysi payt (Kech,Tong,Obed)");
+// // // // // // // // let ism = prompt("Ismingiz")
+// // // // // // // // console.log("Xayrli " + vaqt + ", " + ism)
+             
+
+
+
+
+
+
+
+
+
+
+// // // // // //                 // 1-topshiriq 2-misol
+// // // // // // // function Matnuzunligi(matn) {
+// // // // // // //     return matn.length;
+// // // // // // // }
+// // // // // // // console.log(Matnuzunligi(prompt("Matn kiriting")));        
+
+
+
+
+
+
+
+
+
+
+// // // // // //                 // 1-topshiriq 3-misol
+// // // // // // function shifr(matn) {
+// // // // // //     return matn.replaceAll("a", "@");
+// // // // // // }
+// // // // // // console.log(shifr(prompt("Matn kiriting")));        
+
+
+
+
+
+
+
+
+
+
+
+
+// // // // //                 // 2-topshiriq 1-misol
+// // // // // function chegirma(narx, foiz) {
+// // // // // let yakuniNarx = narx = (narx * foiz /100)
+// // // // //     return yakuniNarx;
+// // // // // }
+// // // // // const narx = prompt("narx")
+// // // // // const foyz = prompt("foyz")
+// // // // // console.log(chegirma(Number( narx), Number (foyz))); 
+
+
+
+
+
+
+
+
+// // // // //                     // 2-topshiriq 2-misol
+// // // // // function yetkazibBerish(summa) {
+// // // // //     if (summa > 100000) {
+// // // // //         return "0 so'm (bepul)";
+// // // // //     } else {
+// // // // //         return "Yetkazib berish pullik";
+// // // // //     }
+// // // // // }
+// // // // // console.log(yetkazibBerish(prompt("olgan narsalaringizni umumiy narxi"))); 
+
+
+
+
+
+
+
+
+
+
+
+// // // //                     // 3-TOPSHIRIQ 
+// // // // function muzqaymoqNarxi(soni) {
+// // // //     return soni * 5000;
+// // // // }
+
+// // // // console.log(muzqaymoqNarxi(prompt("Nechta muzqaymoq olasiz?")));
+
+
+
+
+// // //                         // 4-TOPSHIRIQ
+// // // function vaqtniTop(masofa, tezlik) {
+// // //     return masofa / tezlik;
+// // // }
+
+// // // console.log(vaqtniTop( prompt("Masofani kiriting:")));
+// // //     console.log(vaqtniTop(prompt("Tezlikni kiriting:")));
+
+
+
+// //                     // 5-topshiriq
+// // function chiroqHolati(holat) {
+// //     return holat === "yoq";
+// // }
+
+// // console.log(chiroqHolati(prompt("yoq yoki o'chir deb yozing:")));
+
+
+//                     // 6-TOPSHIRIQ
+                    
+// function perimetr(a) {
+//     return a * 4;
+// }
+
+// console.log(perimetr(prompt("Kvadrat tomonini kiriting:")));
