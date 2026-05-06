59a411a81984cc387d2457981559497316ac1956
bobj0nov <prosto.akkaunt.n2@gmail.com>
2026-05-06 10:36:04 +0500

    Add files via upload

---
+++ Lesson 18/styles.js
@@ -0,0 +1,20 @@
+// Mantiqiy VA (AND - &&)
+console.log(10 > 5 && 8 < 12); // true
+console.log(4 === 4 && 5 > 10);//false
+console.log("Olma" === "Olma" && 20 !== 20);//false
+console.log(true && 7 <= 7);//true
+console.log(0 > 1 && 100 === 100);//false
+console.log(15 >= 10 && "A" !== "B");//true
+console.log(false && true);//false
+console.log(5 + 5 === 10 && 2 * 2 === 4);//true
+console.log(10 > 20 && 5 < 1);//false
+console.log(null === null && undefined === undefined);//true
+
+// Mantiqiy YOKI (OR - ||)
+console.log(5 > 10 || 4 === 4);//true
+console.log(10 < 5 || 1 > 2);//false
+console.log("JS" === "JS" || 5 === 6);//true
+console.log(false || true);//true
+console.log(100 !== 100 || 50 > 10);//true
+console.log(0 === 0 || 7 > 3);//true
+console.log(15 < 10 || 8 >= 9);//false
