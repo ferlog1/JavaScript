9bc312f28f21c817b72727776744f8215afa302f
bobj0nov <prosto.akkaunt.n2@gmail.com>
2026-05-18 11:36:07 +0500

    Add files via upload

---
+++ Lesson 29/stles.js
@@ -0,0 +1,49 @@
+// for(let i = 1 ; i <= 5 ; i++)
+// {console.log("Hozirgi narx " + i*1000 + " " )}
+
+
+
+
+
+
+// let quvvat = 97; 
+// while (quvvat <= 100) {
+//     console.log("Telefon quvvati: " + quvvat + "%");
+//     quvvat++; 
+// }
+
+
+
+
+
+
+// let muzqaymoqBor = false;
+// do {
+//     console.log("Muzqaymoqni 1 marta yaladim");
+// } while (muzqaymoqBor === true); 
+
+// console.log("Sikl tugadi, chunki muzqaymoq  yo'q");
+
+// for (let i = 1; i <= 5; i++) {
+    
+//     if (i === 3) {
+//         continue; 
+//     }
+    
+//     console.log(i);
+// }
+
+
+
+for (let soat = 6; soat <= 10; soat++) {
+    
+   
+    if (soat === 8) {
+        console.log("Soat " + soat + ":00. Men uyg'ondim va budilnikni butunlay o'chirdim! 🛌❌");
+        break;
+    }
+    
+    console.log("Soat " + soat + ":00 Uyg'oning! ⏰");
+}
+
+console.log("Dastur tugadi. ☕");
