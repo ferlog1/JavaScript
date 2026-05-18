f4c34cc3716a5a2f2a4b4afa34cb5c75793e575a
bobj0nov <prosto.akkaunt.n2@gmail.com>
2026-05-18 11:34:53 +0500

    Add files via upload

---
+++ Lesson 26/index.js
@@ -0,0 +1,16 @@
+
+let bexruzObject =
+{
+    name:"Bexruz",
+    age:13,
+    xobi:["gaming"],
+    adress:
+    {
+        city: "Xiva",
+        street:"Samakm",
+    }
+}
+console.log(bexruzObject.adress)
+console.log(bexruzObject.age)
+console.log(bexruzObject.xobi)
+bexruzObject.tel = 212312
