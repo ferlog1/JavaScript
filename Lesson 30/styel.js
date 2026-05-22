daae7229fe7d3501a05f4c17ba3baab0efe807ac
bobj0nov <prosto.akkaunt.n2@gmail.com>
2026-05-22 11:39:33 +0500

    Add files via upload

---
+++ Lesson 30/styel.js
@@ -0,0 +1,80 @@
+// let kun = Number(prompt("Kunni kiriting"));
+
+// switch (kun) {
+//   case 1:
+//     console.log("Dushanba");
+//     break;
+//   case 2:
+//     console.log("Seshanba");
+//     break;
+//   case 3:
+//     console.log("Chorshanba");
+//     break;
+//     case 4:
+//         console.log("Payshanba")
+//         break;
+//         case 5:
+//         console.log("Juma")
+//         break;
+//         case 6:
+//         console.log("Shanba")
+//         break;
+//         case 7:
+//         console.log("Yakshanba")
+//         break;
+        
+
+//   default:
+//     console.log("Bunday kun yo‘q");
+// }
+
+// let oy = Number(prompt("Sonni kiriting"));
+
+// switch(oy) {
+//     case 1:
+//     case 12:
+//     case 2:
+//         console.log("Qish");
+//        break;
+    
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log("Bahor");
+//        break;
+    
+//      case 6:
+//      case 7:
+//      case 8:
+//         console.log("Yoz");
+//        break;
+
+//     case 9:
+//      case 10:
+//      case 11:
+//         console.log("Kuz");
+//        break;
+
+//     default:
+//         console.log("Bunday oy yo'q")
+
+// }
+
+
+let rang = prompt("Rangni kiriting");
+switch (rang) {
+    case "qizil":
+        console.log( "To'xtang!");
+    break;
+
+    case "sariq":
+        console.log( "Tayyorlaning!");
+    break;
+
+    case "yashil":
+        console.log( "Yuring!");
+    break;
+    default:
+        console.log( "Bunday svetofor rangi yo'q")
+
+}
